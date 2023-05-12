aea651470714291d238da9d01d04f53359ba76d2
Fake Name <fakename@fake.com>
2023-05-13 06:31:58 +1000

    Media responsive

--- src/app/Box.tsx
+++ src/app/Box.tsx
@@ -13,13 +13,19 @@ function Torus(
   const mesh = useRef<THREE.Mesh>(null!);
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  useFrame((state, delta) => (mesh.current.rotation.z += delta));
+  //useFrame((state, delta) => (mesh.current.rotation.z += delta));
 
   const texture = new TextureLoader().load(
     "https://threejs.org/examples/textures/water.jpg"
   );
   texture.wrapS = texture.wrapT = RepeatWrapping;
   texture.repeat.set(10, 10);
+
+  useFrame((state, delta) => {
+    if (hovered) {
+      mesh.current.rotation.z += delta; // Adjust the rotation as needed
+    }
+  });
 
   // Use the forwardRef function to forward the ref to the mesh component
   return (
@@ -29,7 +35,8 @@ function Torus(
       scale={active ? 1.5 : 1}
       rotation={[Math.PI / -3, 0, 0]}
       onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
+      onPointerOver={(event) => setHover(true)
+      }
       onPointerOut={(event) => setHover(false)}
     >
       <torusGeometry args={[1, 0.4, 16, 100]} />
@@ -46,50 +53,51 @@ Torus.displayName = 'Torus'
 const ForwardedTorus = React.forwardRef(Torus);
 
 export default function BoxHome() {
-     const torusRef = useRef<THREE.Mesh>(null);
-     const [messageHovered, setMessageHover] = useState(true);
-     const [hoverCount, setHoverCount] = useState(0);
-    const handleTorusHover = () => {
-      if (hoverCount === 0) {
-        setMessageHover(false);
-      }
-      setHoverCount(hoverCount + 1);
-    };
-    return (
-      <>
-        <div
-          className={styles.CContainer}
-          onPointerEnter={(event) => setMessageHover(true)}
-        >
-          <div className={styles.mmenuMessage}>
-            {!messageHovered ? (
-              <h2>{hoverCount === 1 ? "Menu Item One" : "Menu Item Two"}</h2>
-            ) : (
-              <h2>mouse over torus to see menu items</h2>
-            )}
-          </div>
-          <Canvas>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <ForwardedTorus
-              ref={torusRef}
-              position={[0, 0, 0]}
-              onPointerEnter={handleTorusHover}
-            />
-
-            {/* <Box position={[1.2, 0, 0]} /> */}
-
-            <Stars
-              radius={100}
-              depth={50}
-              count={5000}
-              factor={8}
-              saturation={0}
-              fade
-              speed={1}
-            />
-          </Canvas>
+  const torusRef = useRef<THREE.Mesh>(null);
+  const [messageHovered, setMessageHover] = useState(true);
+  const [hoverCount, setHoverCount] = useState(0);
+  const handleTorusHover = () => {
+    
+    if (hoverCount === 0) {
+      setMessageHover(false);
+    }
+    setHoverCount(hoverCount + 1);
+  };
+  return (
+    <>
+      <div
+        className={styles.CContainer}
+        onPointerEnter={(event) => setMessageHover(true)}
+      >
+        <div className={styles.mmenuMessage}>
+          {!messageHovered ? (
+            <h2>{hoverCount === 1 ? "Menu Item One" : "Menu Item Two"}</h2>
+          ) : (
+            <h2>mouse over torus to see menu items</h2>
+          )}
         </div>
-      </>
-    );
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <ForwardedTorus
+            ref={torusRef}
+            position={[0, 0, 0]}
+            onPointerEnter={handleTorusHover}
+          />
+
+          {/* <Box position={[1.2, 0, 0]} /> */}
+
+          <Stars
+            radius={100}
+            depth={50}
+            count={5000}
+            factor={8}
+            saturation={0}
+            fade
+            speed={1}
+          />
+        </Canvas>
+      </div>
+    </>
+  );
 }
